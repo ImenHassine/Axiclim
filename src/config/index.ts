9401d9ae764af1b0054741ffe0415bfaa902e911
imen <i.hassine@vitah.tech>
2023-12-11 16:06:08 +0100

    aDD CONFIG

---
+++ src/config/index.ts
@@ -0,0 +1,21 @@
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+export type Configuration = {
+  NODE_ENV: string;
+  PORT: number;
+  WIPSOS_URL: string;
+  APP_DATABASE_URL: string;
+  APP_LOG_LEVEL: string;
+};
+
+let currentConfig: Configuration = {
+    NODE_ENV: process.env.NODE_ENV || 'production',
+    PORT: +(process.env.PORT || 3000),
+    WIPSOS_URL: process.env.WIPSOS_URL || 'http://wipsos-extranet.com',
+    APP_DATABASE_URL: process.env.APP_DATABASE_URL || '',
+    APP_LOG_LEVEL: process.env.APP_LOG_LEVEL || 'info'
+  }
+
+export { currentConfig as config }
